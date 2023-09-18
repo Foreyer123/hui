2b1c52da611451d2f5ba725ad7c204573bcb73d8
Foreyer123 <139108781+Foreyer123@users.noreply.github.com>
2023-09-18 14:35:00 +0800

    Add files via upload

---
+++ js/guangzhou.js
@@ -0,0 +1,11 @@
+// let index = 0;
+// let timer;
+// run = 0;
+// function run() {
+//     if( index <= -3200){
+//         index = 0;
+//     }
+//     imgList.style.marginLeft = index + 'px';
+//     index -=10;
+//     timer = setTimeout(run,0);
+// }
